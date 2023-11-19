1f75a66de8e7b8792f0dd40bc19d91c323e3f8e4
harsh1634 <135155969+harsh1634@users.noreply.github.com>
2023-11-20 00:54:25 +0530

    Add files via upload

---
+++ Final Codes/A2) Pass2 Assembler/LT.java
@@ -0,0 +1,13 @@
+
+public class LT {
+	String lit_name;
+	int addr,leng;
+	char rel;
+	LT(String n,int a,int l,char c)
+	{
+		lit_name=n;
+		addr=a;
+		leng=l;
+		rel=c;
+	}
+}
---
+++ Final Codes/A2) Pass2 Assembler/ST.java
@@ -0,0 +1,13 @@
+
+public class ST {
+String Sym_name;
+int addr,leng;
+char rel;
+ST(String n,int a,int l,char c)
+{
+	Sym_name=n;
+	addr=a;
+	leng=l;
+	rel=c;
+}
+}
